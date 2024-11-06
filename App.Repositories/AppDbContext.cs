d527ad4c7690723d63a57446d2e770c5dc95ac4d
Gokhan Kus <gkus1998@hotmail.com>
2024-11-06 13:29:06 +0300

    AppDbContext, product entity and config

---
+++ App.Repositories/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace App.Repositories;
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options) //primary ctor with .net 8.0
+{
+	public DbSet<Product> Products { get; set; } = default!;
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+		base.OnModelCreating(modelBuilder);
+	}
+}
+
---
+++ App.Repositories/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace App.Repositories;
+public class Product
+{
+	public int Id { get; set; }
+	public string Name { get; set; } = default!; //default deger (null) olmayacak
+	public decimal Price { get; set; }
+	public int Stock { get; set; }
+}
+
