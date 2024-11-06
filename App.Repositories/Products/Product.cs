7434d5ed992e0dd5ed47eb21c3089b3574b1177d
Gokhan Kus <gkus1998@hotmail.com>
2024-11-06 15:49:03 +0300

    GenericRepository concrete cs and its interface

---
+++ App.Repositories/Products/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace App.Repositories.Products;
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!; //default deger (null) olmayacak
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+}
