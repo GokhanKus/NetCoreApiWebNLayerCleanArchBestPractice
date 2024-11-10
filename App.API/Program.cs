55c05030396ecf97be185a23f11a3d3d658e72d4
Gokhan Kus <gkus1998@hotmail.com>
2024-11-10 11:56:12 +0300

    Add Fluent Validation

--- App.API/Program.cs
+++ App.API/Program.cs
@@ -1,5 +1,6 @@
 using App.Repositories.Extensions;
 using App.Services.Extensions;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//.net'in default validation filterini kapattik, cunku biz fluent validation kullanacagiz.
+builder.Services.Configure<ApiBehaviorOptions>(options => options.SuppressModelStateInvalidFilter = true);
 builder.Services.AddRepositories(builder.Configuration).AddServices();
 
 var app = builder.Build();
--- App.Services/Extensions/ServiceExtension.cs
+++ App.Services/Extensions/ServiceExtension.cs
@@ -1,5 +1,8 @@
 ﻿using App.Services.Products;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace App.Services.Extensions;
 public static class ServiceExtension
@@ -7,6 +10,10 @@ public static class ServiceExtension
 	public static IServiceCollection AddServices(this IServiceCollection services)
 	{
 		services.AddScoped<IProductService, ProductService>();
+
+		services.AddFluentValidationAutoValidation();
+		services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
 		return services;
 	}
 }
